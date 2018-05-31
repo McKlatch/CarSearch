1a3f5669a8d511fe69ca23b35de9afab87ae5a2b
McKlatch <david@hawkandlime.com>
2018-05-31 21:06:06 +0100

    Initial Commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Let\'s find the car you\'re looking for:'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
