dbf8e158f47da5bcde0a1cc45ebf2640ec999b6c
McKlatch <david@hawkandlime.com>
2018-05-31 21:29:06 +0100

    Connect database to list

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="hello">
     <h2>{{ msg }}</h2>
+    <car-list/>
   </div>
 </template>
 
 <script>
+import carList from './vehicles/list.vue'
+
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Let\'s find the car you\'re looking for:'
     }
-  }
+  },
+  components: { carList }
 }
 </script>
 
--- src/components/vehicles/list.vue
+++ src/components/vehicles/list.vue
@@ -1,7 +1,27 @@
 <template>
+  <div>
+    <p v-for="vehicle in vehicles">
+      {{ vehicle.vehicleCapDetails.presentationMake }}
+      {{ vehicle.vehicleCapDetails.presentationRange }}
+    </p>
+  </div>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      vehicles: {}
+    }
+  },
+  created () {
+    fetch('/static/db.json')
+    .then(blob => blob.json())
+    .then(json => {
+      this.vehicles = json.vehicles
+    })
+  }
+}
 </script>
 
 <style>
