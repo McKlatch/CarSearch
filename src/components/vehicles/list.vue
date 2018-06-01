9f3672d5e3ee6813c616434c63e06231e265f143
McKlatch <david@hawkandlime.com>
2018-06-01 18:00:03 +0100

    Add typeahead search on list

--- src/components/vehicles/list.vue
+++ src/components/vehicles/list.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p v-for="vehicle in allVehicles">
+    <input v-model="queryStr" type="text" name="carSearch">
+    <p v-for="vehicle in searchVehicles">
       <car-card :vehicle="vehicle" />
     </p>
   </div>
@@ -11,7 +12,18 @@ import carCard from './Car.vue'
 import { loadDatabase } from '../mixins/loadDatabase'
 
 export default {
+  data () {
+    return {
+      queryStr: ''
+    }
+  },
   computed: {
+    searchVehicles () {
+      return this.allVehicles.filter(index => {
+        const regex = new RegExp(this.queryStr, 'gi')
+        return index.vehicleCapDetails.presentationMake.match(regex) || index.vehicleCapDetails.presentationRange.match(regex)
+      })
+    },
     makes () {
       return [...new Set(this.allVehicles.map(carMakes => carMakes.vehicleCapDetails.presentationMake))]
     }
